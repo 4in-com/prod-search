0c6860756a0ce6c281c1a9caa74895fe2aebc0d7
russel muyembe <russelmuyembe9@gmail.com>
2026-01-15 17:47:15 +0200

    Update cronJob.js

--- cronJob.js
+++ cronJob.js
@@ -4,7 +4,7 @@ const CronJob = require('cron').CronJob;
 // every day at 2am
   console.log('Running crawl schedule...');
   sites.forEach(site => crawlSite(site));
-new CronJob('0 2 * * *', () => {
-  console.log('Running crawl schedule...');
-  sites.forEach(site => crawlSite(site));
-}).start();
+// new CronJob('0 2 * * *', () => {
+//   console.log('Running crawl schedule...');
+//   sites.forEach(site => crawlSite(site));
+// }).start();
